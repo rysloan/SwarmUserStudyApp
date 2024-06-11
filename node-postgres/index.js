f4bb5892df67bd290f713fcac50794f5337480ae
u1261748@eng.utah.edu <u1261748@eng.utah.edu>
2024-06-11 12:23:39 -0700

    added procfile

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -4,6 +4,16 @@ const port = 3001
 
 const merchant_model = require('./merchantModel')
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../video-picker/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../video-picker/build/index.html'))
+})
+
 app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
