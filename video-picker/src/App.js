54eb310555d2ce9f98aea9c94ec16b09512e7916
Ryan Sloan <rypsloan@gmail.com>
2024-08-14 10:40:26 -0700

    added test queries

--- video-picker/src/App.js
+++ video-picker/src/App.js
@@ -15,28 +15,58 @@ function getRandomNumInList(size) {
 
 function App() {
 
+  function getGifNumber(gifPath) {
+    let split = gifPath.split('/')
+    let split2 = split[split.length - 1].split('.')
+    return split2[0]
+  }
+
+  /**
+   * Get X amount of gifs from the ./SwarmVideos file and returns them as an array
+   * 
+   * @param count amount of gifs to grab and return
+   * 
+   * @returns an array of gifs with length equal to count
+   */
   function getXGifs(count) {
     let index = 0;
     let arr = []
     for(let i = 0; i < count; i++) {
       index = getRandomNumInList(tempvids.length)
-      arr.push(tempvids[index])
+
+      let gifNumber = getGifNumber(tempvids[index]);
+      arr.push(gifNumber);
+
     }
     return arr;
   }
 
-  let unpicked = "grey solid 7px"
-  let picked = "green solid 7px"
-  const storedQueryCount = parseInt(JSON.parse(localStorage.getItem("queryCount")))
+  function userTest() {
+    // pick two of the same gif
+    let index = getRandomNumInList(tempvids.length)
+    let arr = []
 
-  const [sceneSelected, setSceneSelected] = useState(false)
-  const [start, setStart] = useState(true)
-  const [firstGif, setFirstGif] = useState([])
-  const [secondGif, setSecondGif] = useState([])
-  const [thirdGif, setThirdGif] = useState([])
-  const [queryCount, setQueryCount] = useState(storedQueryCount)
-  const [finished, setFinished] = useState(false)
-  
+    let gifNumber = getGifNumber(tempvids[index]);
+    arr.push(gifNumber)
+
+    let index2 = index;
+    while (index2 === index) {
+      index2 = getRandomNumInList(tempvids.length)
+    }
+
+    gifNumber = getGifNumber(tempvids[index2]);
+    arr.push(gifNumber)
+
+    return arr;
+  }
+
+  /**
+   * Creates a gif object 
+   * 
+   * @param props details about the gif to create (prop.gif = the gif data | props.handler = the gif setter method)
+   * 
+   * @returns a gif object
+   */
   function Gif(props) {
     let info = props.gif
     if (start) 
@@ -47,7 +77,7 @@ function App() {
           alt='loading...' 
           height={250} 
           width={250} 
-          style={{border: info[1], margin: '10px 10px auto'}}
+          style={{border: info[1], margin: '10px 10px auto', transform: 'rotateX(90)'}}
           onClick={() => {
             if (info[1] === unpicked && !sceneSelected) {
               props.handler([info[0], picked])
@@ -62,8 +92,9 @@ function App() {
     )
   }
 
-  const [prolificId, setProlificId] = useState();
-
+  /**
+   * Gets the prolific id of the user from the url parameters, if none is found then default to testID
+   */
   function getProlificId() {
     const url = window.location.href;
     const split = url.split('?');
@@ -76,6 +107,16 @@ function App() {
     setProlificId(id);
   }
 
+  /**
+   * Creates a new row in the backend database
+   * 
+   * @param gif1 The 
+   * @param gif2
+   * @param gif3
+   * @param gif1
+   * 
+   * @returns a gif object
+   */
   function createBehaviorData(gif1, gif2, gif3, gifSelected) {
     let selected = gifSelected
     let prolificid = prolificId
@@ -91,11 +132,25 @@ function App() {
       });
   }
 
+  let unpicked = "grey solid 7px"
+  let picked = "green solid 7px"
+  const storedQueryCount = parseInt(JSON.parse(localStorage.getItem("queryCount")))
+
+  const [sceneSelected, setSceneSelected] = useState(false)
+  const [start, setStart] = useState(true)
+  const [firstGif, setFirstGif] = useState([])
+  const [secondGif, setSecondGif] = useState([])
+  const [thirdGif, setThirdGif] = useState([])
+  const [queryCount, setQueryCount] = useState(storedQueryCount)
+  const [finished, setFinished] = useState(false)
+  const [prolificId, setProlificId] = useState();
+  const [testIdealAnswer, setTestIdealAnswer] = useState(-1);
+
   useEffect(() => {
 
     localStorage.setItem("queryCount", JSON.stringify(queryCount))
 
-    if (queryCount >= 10) {
+    if (queryCount >= 50) {
       alert("You have finished the study, Thank you! Completion Code: CK53M54P")
       setStart(true);
       setFinished(true);
@@ -119,12 +174,10 @@ function App() {
           onClick={() => {
             // Change Button From START -> NEXT
             if (start) {
-              // localStorage.setItem("queryCount", JSON.stringify(0))
               setStart(false)
               getProlificId()
             }
             else {
-              // Save data to database
               let selected = -1;
               if (firstGif[1] === picked)
                 selected = 1
@@ -133,32 +186,59 @@ function App() {
               else if (thirdGif[1] === picked)
                 selected = 3
 
-              createBehaviorData(firstGif[0], secondGif[0], thirdGif[0], selected)
-              // let queryCount = JSON.parse(localStorage.getItem("queryCount"))
-              // let number = parseInt(queryCount)
-              // let temp = number + 1
-              let temp = queryCount + 1;
-              setQueryCount(temp);
-              // if (queryCount >= 9) {
-              //   alert("You have finished the study, Thank you! Completion Code: CK53M54P")
-              //   setStart(true);
-              //   setFinished(true);
-              // }
-              // localStorage.setItem("queryCount", JSON.stringify(temp))
-              // console.log(temp)
-              // if (temp >= 9) {
-              //   alert("You have finished the study, Thank you! Completion Code: CK53M54P")
-              //   setStart(true);
-              //   setFinished(true);
-              // }
+              if (testIdealAnswer !== -1) {
+                // Save data to database
+                let goodTester = false;
+                if (selected === testIdealAnswer)
+                  goodTester = true;
+
+                // TODO : Need to trucate the gif to just the number and not the entire path
+                createBehaviorData(firstGif[0], secondGif[0], thirdGif[0], selected)
+                let temp = queryCount + 1;
+                setQueryCount(temp);
+                setTestIdealAnswer(-1);
+              }
+              else {
+                createBehaviorData(firstGif[0], secondGif[0], thirdGif[0], selected)
+                let temp = queryCount + 1;
+                setQueryCount(temp);
+              } 
             }
 
-            // Repopulate gifs with new gifs
-            let temp = getXGifs(3)
-            setFirstGif([temp[0], unpicked])
-            setSecondGif([temp[1], unpicked])
-            setThirdGif([temp[2], unpicked])
-
+            if (queryCount % 15 === 0) {
+              // Run the Test Query
+              let temp = userTest();
+              let random = Math.floor(Math.random() * 3);
+              switch(random) {
+                case 0:
+                  setTestIdealAnswer(1);
+                  setFirstGif([temp[1], unpicked])
+                  setSecondGif([temp[0], unpicked])
+                  setThirdGif([temp[0], unpicked])
+                  break;
+                case 1:
+                  setTestIdealAnswer(2);
+                  setFirstGif([temp[0], unpicked])
+                  setSecondGif([temp[1], unpicked])
+                  setThirdGif([temp[0], unpicked])
+                  break;
+                case 2:
+                  setTestIdealAnswer(3);
+                  setFirstGif([temp[0], unpicked])
+                  setSecondGif([temp[0], unpicked])
+                  setThirdGif([temp[1], unpicked])
+                  break;
+                default:
+              }
+            }
+            else {
+              // Repopulate gifs with new gifs
+              let temp = getXGifs(3)
+              setFirstGif([temp[0], unpicked])
+              setSecondGif([temp[1], unpicked])
+              setThirdGif([temp[2], unpicked])
+            }
+        
             // Reset Scene Selected
             setSceneSelected(false)
           }}
